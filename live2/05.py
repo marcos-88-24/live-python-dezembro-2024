e718103fc66906e295bac8e91e8cce303871067e
marcos-88-24 <mrcssantos174@gmail.com>
2024-12-16 09:03:21 -0300

    Create 05.py

---
+++ live2/05.py
@@ -0,0 +1,12 @@
+nota = 9
+preco = 4.5
+nome_cliente = "Fernando Leonid"
+pagou = True 
+
+# array -> lista
+produtos = ["teclado", "mouse", "monitor"]
+
+contador = 0
+while contador <= 2:
+    print (produtos[contador])
+    contador += 1
