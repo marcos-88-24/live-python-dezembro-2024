85061a2c8c3bbf964c93058614ff3fb8c01e2f69
marcos-88-24 <mrcssantos174@gmail.com>
2024-12-16 09:02:55 -0300

    Create 04.py

---
+++ live2/04.py
@@ -0,0 +1,21 @@
+
+opcao = ''
+while opcao != '0':
+    print ("1 - Nome do Aluno")
+    print ("2 - Nota do Aluno")
+    print ("3 - Situação do Aluno")
+    print ("0 - Sair")
+    opcao = input ("Digite uma opção: ")
+    if opcao == '1':
+        print ("Fernando Leonid")
+        input ("digite enter para continuar...")
+    elif opcao == '2':
+        print ("7")
+        input ("digite enter para continuar...")
+    elif opcao  == '3':
+        print ("Aprovado!")
+        input ("digite enter para continuar...")
+    elif opcao == '0':
+        print ("Saindo do sistema...")
+    else:
+        print ("Opção errada, tente novamente!")
